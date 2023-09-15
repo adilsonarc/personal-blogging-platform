b0f4a14df75565a056fe876a31e5266f16009726
adishow <adilsoncabraldev@gmail.com>
2023-09-15 21:45:15 +0100

    Backend Spring init

---
+++ backend/src/main/java/adilsonarc/portfolio/blog/PersonalBloggingPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package adilsonarc.portfolio.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBloggingPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBloggingPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/adilsonarc/portfolio/blog/PersonalBloggingPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package adilsonarc.portfolio.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBloggingPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
